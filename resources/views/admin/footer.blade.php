1a32ba2bbeafc45ee8d9249babc2499dceb0a181
ss-brar <ss-brar>
2021-08-05 12:15:02 +0530

    fresh project setup

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,31 @@
+ <!-- Main Footer -->
+ <footer class="main-footer">
+    <strong>Copyright &copy; 2021 <a href="https://richestsoft.com/">Richestsoft.com</a>.</strong>
+    All rights reserved.
+    <!-- <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.1.0
+    </div> -->
+  </footer>
+</div>
+<!-- ./wrapper -->
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('js/adminlte.js')}}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{asset('plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+<script src="{{asset('plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+<script src="{{asset('plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('plugins/chart.js/Chart.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('js/demo.js')}}"></script>
+<script src="{{asset('js/pages/dashboard2.js')}}"></script>
